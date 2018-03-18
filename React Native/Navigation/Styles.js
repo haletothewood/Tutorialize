b00e0c852500ad8534a56881d41687d4fa394c6c
haletothewood <davidnhalewood@hotmail.com>
2018-03-18 12:44:58 +0000

    Folder restructoring

---
+++ React Native/Navigation/Styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native'
+
+export default Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#2e3440',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      color: '#F5FCFF',
+      margin: 10,
+    },
+    example: {
+      textAlign: 'center',
+      color: '#B48EAD',
+      fontSize: 30,
+      fontWeight: "200",
+      marginBottom: 5,
+    },
+    textinput: {
+      fontSize: 20,
+      color: '#a3be8c',
+      height: 50,
+      width: 200,
+      borderBottomColor: '#F5FCFF',
+      textAlign: 'center'
+    }
+})
