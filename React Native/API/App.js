9ab3707fa7d4c05d7664ee93f6b7b0fef7491f34
haletothewood <davidnhalewood@hotmail.com>
2018-03-18 13:10:24 +0000

    Initial setup of API app

--- React Native/API/App.js
+++ React Native/API/App.js
@@ -1,58 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import Styles from './Styles'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonPressed: false
+    }
+  }
+
+  handleOnPress = () => {
+    this.setState({
+      buttonPressed: true
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      this.state.buttonPressed ? 
+      <View style={Styles.container}>
+      <Text style={Styles.welcome}>
+        This needs implementing
+      </Text>
       </View>
-    );
+      :
+      <View style={Styles.container}>
+        <Text style={Styles.welcome}>
+          Welcome to Tutorialize!
+        </Text>
+        <Text style={Styles.example}>
+          API's
+        </Text>
+        <Text style={Styles.instructions}>
+          In order to see it in action please click the button...
+        </Text>
+        <Button
+        title='Press'
+        onPress={this.handleOnPress}
+        />
+      </View>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
