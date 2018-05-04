dbc88c8ba384bbde57e0f1cd0bd8fe53dd983258
David Halewood <davidnhalewood@hotmail.com>
2018-05-04 14:42:57 +0100

    Add DTO for Author and map from Entity

--- API/ASPdotnet/src/Library.API/Controllers/AuthorsController.cs
+++ API/ASPdotnet/src/Library.API/Controllers/AuthorsController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Library.API.Helpers;
 
 namespace Library.API.Controllers
 {
+    [Route("api/authors")]
     public class AuthorsController : Controller
     {
         private ILibraryRepository _libraryRepository;
@@ -17,11 +20,21 @@ namespace Library.API.Controllers
             _libraryRepository = libraryRepository;
         }
 
-        [HttpGet("api/authors")]
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _libraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+
+            var authors = authorsFromRepo.Select(author => new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                })
+                .ToList();
+
+            return new JsonResult(authors);
         }
     }
 }
